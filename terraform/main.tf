2fe24085f23d1cb5b03e0034a09e19a6b1902b62
Enol Fernandez <enol.fernandez@egi.eu>
2020-11-26 15:20:20 +0000

    Add the IP as output

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,9 +16,8 @@ resource "openstack_networking_floatingip_v2" "public_ip" {
 }
 
 resource "openstack_compute_instance_v2" "cloud-info" {
-  name     = "cloud-info"
-  image_id = "0a3d51ae-56e9-4492-ba7f-0163ce3f0708"
-  # 2 cores 4 GB RAM
+  name            = "cloud-info"
+  image_id        = "0a3d51ae-56e9-4492-ba7f-0163ce3f0708"
   flavor_id       = 3
   security_groups = ["default", "ssh"]
   key_pair        = "enol"
@@ -30,4 +29,8 @@ resource "openstack_compute_instance_v2" "cloud-info" {
 resource "openstack_compute_floatingip_associate_v2" "fip" {
   floating_ip = openstack_networking_floatingip_v2.public_ip.address
   instance_id = openstack_compute_instance_v2.cloud-info.id
+}
+
+output "floating_ip" {
+  value = openstack_networking_floatingip_v2.public_ip.address
 }
